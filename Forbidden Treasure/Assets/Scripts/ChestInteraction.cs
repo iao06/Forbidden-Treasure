c077613fe210c7ed7c9ab959afc6b43cab0bf37e
iao06 <128407783+iao06@users.noreply.github.com>
2023-05-01 10:32:51 -0700

    TREASURE CHESTS!!!
    -Added Treasure Chests and its Animations
    -Added Coin animations (it just spins freely)

---
+++ Forbidden Treasure/Assets/Scripts/ChestInteraction.cs
@@ -0,0 +1,40 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ChestInteraction : MonoBehaviour
+{
+    public bool chestInteract;
+    Animator animr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animr = gameObject.GetComponent<Animator>();
+        chestInteract = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chestInteract == true)
+        {
+            animr.SetBool("chestInteracted", true);
+        }
+        
+        if (chestInteract == false)
+        {
+            animr.SetBool("chestInteracted", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision)
+        {
+            chestInteract = true;
+            ScoreTextScipt.coinAmount += 50;
+        }
+    }
+}
