d9a6eff4aa31629bee311c4d77b62470187cd856
iao06 <isaiaholiva06@gmail.com>
2023-05-21 13:10:58 -0700

    Finished Boss attacks
    - boss attacks are now finished
    - need to test but i dont got time right now

---
+++ Forbidden Treasure/Assets/Scripts/BossShooting.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+    private GameObject player;
+
+    Animator anim;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GameObject.FindGameObjectWithTag("Boss").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distance < 10 && anim.GetBool("isPhase3"))
+        {
+            timer += Time.deltaTime;
+
+            if (timer > 2)
+            {
+                timer = 0;
+
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
--- Forbidden Treasure/Assets/Scripts/BulletBehaviour.cs
+++ Forbidden Treasure/Assets/Scripts/BulletBehaviour.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
+    private GameObject player;
+    private Rigidbody2D rb;
+
+    public float force;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rotation = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Deg2Rad;
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+            Destroy(gameObject);
+        }
     }
 }
